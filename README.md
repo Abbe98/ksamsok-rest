f07f290ebe554c30475b25c8199533e460963926
abbe98 <albin.post@gmail.com>
2017-09-07 13:55:22 +0200

    That is it

---
+++ README.md
@@ -0,0 +1,60 @@
+# K-Samsök REST
+
+K-Samsök REST is an JSON REST API built as a proxy for the K-Samsök (Swedish Open Cultural Heritage) API.
+
+## API Documentation
+
+There is a public instance of this API at `https://tools.wmflabs.org/ksamsok-rest`.
+
+### Records
+
+Retrieve a single record from K-Samsök. An URI is usually formated as `<institution>/dataset/id`.
+
+```
+/records/<uri>
+```
+
+Retrieve all relations for a given record.
+
+```
+/records/<uri>/relations
+```
+
+Search records by text.
+
+```
+/records?action=search&text=<string>
+```
+
+There is three optional parameters available for the search action. `start` and `hits` for pagination. `image` can be set to true to only include records with images.
+
+Search by text limited to records with images while skiping the first five results and limit the number of responses to ten:
+
+```
+/records?action=search&text=<string>&image=true&hits=10&start=5
+```
+
+Search by bounding box:
+
+```
+/records?action=bbox&west=<int|decimal>&south=<int|decimal>&east=<int|decimal>&north=<int|decimal>
+```
+
+Note that the API can handle both SWEREF 99 and WGS84. `start` and `hits` are available for bounding searches too.
+
+### Search Hints
+
+Returns a list of five search hints for the given string. Commonly used for auto complete features.
+
+```
+/hints/<string>
+```
+
+## Setup
+
+```bash
+git clone https://github.com/Abbe98/ksamsok-rest.git
+cd ksamsok-rest
+pipenv install
+pipenv run python3 app.py
+```
--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 import sys
+import configparser
 from ksamsok import KSamsok
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, abort
 
 # http://www.ianbicking.org/illusive-setdefaultencoding.html
 if sys.version[0] == '2':
@@ -18,3 +19,61 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Methods', 'GET')
     return response
+
+class Record(Resource):
+    def get(self, uri):
+        record = soch.getObject(uri)
+
+        if record:
+            return record
+
+        return abort(404, message='Record {} doesn\'t exist'.format(uri))
+
+api.add_resource(Record, '/records/<path:uri>')
+
+class RecordRelations(Resource):
+    def get(self, uri):
+        relations = soch.getRelations(uri)
+
+        if relations:
+            return relations
+
+        return abort(404, message='Record {} doesn\'t exist'.format(uri))
+
+api.add_resource(RecordRelations, '/records/<path:uri>/relations')
+
+class Records(Resource):
+    def get(self):
+        args = request.args
+
+        if 'action' in args and args['action'] == 'bbox':
+            if all(i in ['west', 'south', 'east', 'north'] for i in args):
+                start = args['start'] if 'start' in args else 0
+                hits = args['hits'] if 'hits' in args else 50
+
+                return soch.geoSearch(args['west'], args['south'], args['east'], args['north'], start, hits)
+
+            return abort(400, message='One or multiple parameters are missing.')
+
+        if 'action' in args and args['action'] == 'search':
+            if 'text' in args:
+                start = args['start'] if 'start' in args else 0
+                hits = args['hits'] if 'hits' in args else 50
+                images = True if 'image' in args else False
+
+                return soch.search(request.args['text'], start, hits, images)
+
+            return abort(400, message='The text parameter is missing.')
+
+        return abort(400, message='The action parameter is missing or has a invalid value.')
+
+api.add_resource(Records, '/records')
+
+class Hints(Resource):
+    def get(self, text):
+        return soch.getHints(text)
+
+api.add_resource(Hints, '/hints/<string:text>')
+
+if __name__ == '__main__':
+    app.run()
