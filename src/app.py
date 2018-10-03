0c3aebf99981686cfb06477565fd69ca6aa044af
Abbe98 <albin.post@gmail.com>
2018-10-03 11:35:57 +0200

    add X-Powered-By header

--- src/app.py
+++ src/app.py
@@ -22,6 +22,7 @@ soch = KSamsok('test')
 def after_request(resp):
     resp.headers.add('Access-Control-Allow-Origin', '*')
     resp.headers.add('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS')
+    resp.headers.add('X-Powered-By', 'K-Samsök REST Version 1.1.0')
     return resp
 
 class Record(Resource):
