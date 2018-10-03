de5e38e4d72004520416d9fdd3612be8d108a586
Abbe98 <albin.post@gmail.com>
2018-10-03 10:47:13 +0200

    fix broken headers

--- src/app.py
+++ src/app.py
@@ -17,7 +17,7 @@ soch = KSamsok('test')
 @app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Methods', 'GET', 'HEAD', 'OPTIONS')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS')
     return response
 
 class Record(Resource):
